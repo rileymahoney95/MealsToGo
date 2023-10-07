eb34178d5dea6be8b092fe30e35b11b04f822381
rileymahoney95 <rileyrmahoney@gmail.com>
2023-10-07 11:35:04 -0400

    adding scrolling

---
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -0,0 +1,63 @@
+import React from "react";
+
+import { SvgXml } from "react-native-svg";
+
+import { Spacer } from "../../../components/spacer/spacer.component";
+import { Text } from "../../../components/typography/text.component";
+
+import star from "../../../../assets/star";
+import open from "../../../../assets/open";
+
+import {
+  RestaurantCard,
+  RestaurantCardCover,
+  Info,
+  Section,
+  SectionEnd,
+  Rating,
+  Icon,
+  Address,
+} from "./restaurant-info-card.styles";
+
+export const RestaurantInfoCard = ({ restaurant = {} }) => {
+  const {
+    id,
+    name = "Zona Fresca",
+    icon,
+    photos = [
+      "https://zonafresca.com/images/gallery/ftlaud_exterior6569_690.jpg",
+    ],
+    address = "635 N Federal Hwy",
+    isOpenNow = true,
+    rating = 5,
+    isClosedTemporarily = false,
+  } = restaurant;
+
+  const ratingArray = Array.from(new Array(Math.floor(rating)));
+
+  return (
+    <RestaurantCard elevation={5}>
+      <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
+      <Info>
+        <Text variant="label">{name}</Text>
+        <Section>
+          <Rating>
+            {ratingArray.map(() => (
+              <SvgXml xml={star} width={20} height={20} />
+            ))}
+          </Rating>
+          <SectionEnd>
+            {isClosedTemporarily && <Text variant="error">CLOSED TEMPORARILY</Text>}
+            <Spacer position="left" size="large">
+              {isOpenNow && <SvgXml xml={open} width={20} height={20} />}
+            </Spacer>
+            <Spacer position="left" size="large">
+              <Icon></Icon>
+            </Spacer>
+          </SectionEnd>
+        </Section>
+        <Address>{address}</Address>
+      </Info>
+    </RestaurantCard>
+  );
+};
